69ba01a5bdd6ed4c631b0756a2d394d3f52bc54e
HugoDemont <hugodemont62@hotmail.fr>
2024-01-24 23:26:09 +0100

    [FEAT] Improve user info display in ProfileScreen
    The code implements enhancement features in the ProfileScreen. It now displays additional user information including the username, the identity provider, and the user ID. If a user has a set photo URL, an image of the user will also be shown.

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, {useEffect, useState} from 'react';
+import {Button, Text, View} from 'react-native';
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
 
 const ProfileScreen = ({navigation}) => {
   const [user, setUser] = useState(null);
@@ -21,6 +21,11 @@ const ProfileScreen = ({navigation}) => {
   return (
     <View>
       <Text>Bonjour, {user.email}</Text>
+      <Text>Nom d'utilisateur: {user.displayName}</Text>
+      <Text>Fournisseur d'identité: {user.providerData[0].providerId}</Text>
+      <Text>UID: {user.uid}</Text>
+      {user.photoURL &&
+        <Image source={{uri: user.photoURL}} style={{width: 50, height: 50}}/>}
       <Button title="Se déconnecter" onPress={() => {
         const auth = getAuth();
         auth.signOut();
@@ -28,6 +33,6 @@ const ProfileScreen = ({navigation}) => {
       }}/>
     </View>
   );
-}
+};
 
 export default ProfileScreen;
