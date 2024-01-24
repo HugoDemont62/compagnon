1d1ca3049a062521721386c659d673624bd0610d
HugoDemont <91455631+HugoDemont62@users.noreply.github.com>
2024-01-24 12:17:31 +0100

    [DOCS] Add todo list for next time

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -5,6 +5,7 @@ export default function ProfileScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Profil Screen</Text>
+  // TODO : faire la page profil avec les informations du user 
     </View>
   );
 }
