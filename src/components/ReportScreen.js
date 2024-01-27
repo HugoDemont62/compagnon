815433f07bc4593d930850cb02ae33c63e79c24f
HugoDemont <hugodemont62@hotmail.fr>
2024-01-27 12:02:39 +0100

    [FEAT] Add ReportScreen and navigation updates
    Implemented a new ReportScreen component and integrated it in the app's navigation structure. HomeScreen was modified to import TouchableOpacity from 'react-native'. Various dependencies for PDF generation like expo-print and react-native-pdf-lib were included in package.json.

--- src/components/ReportScreen.js
+++ src/components/ReportScreen.js
@@ -1,11 +1,92 @@
-import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+const ReportScreen = ({navigation}) => {
+  const [report, setReport] = useState({
+    horaire: '',
+    chantier: '',
+    jourNuit: '',
+    date: '',
+    meteo: '',
+    temperature: '',
+    semaine: '',
+    travaux: '',
+    unite: '',
+    quantite: '',
+  });
+
+  const handleInputChange = (name, value) => {
+    setReport({...report, [name]: value});
+  };
+
+  const handleSubmit = () => {
+    // Ajouter le code pour générer le PDF
+    console.log(report);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Rapport Screen</Text>
+      <TextInput style={styles.textInput}
+                 value={report.horaire}
+                 onChangeText={(text) => handleInputChange('horaire', text)}
+                 placeholder="Horaire"
+      />
+      <TextInput style={styles.textInput}
+                 value={report.chantier}
+                 onChangeText={(text) => handleInputChange('chantier', text)}
+                 placeholder="Chantier"
+      />
+      <TextInput style={styles.textInput}
+                 value={report.jourNuit}
+                 onChangeText={(text) => handleInputChange('jourNuit', text)}
+                 placeholder="Jour ou Nuit"
+      />
+      <TextInput style={styles.textInput}
+                 value={report.date}
+                 onChangeText={(text) => handleInputChange('date', text)}
+                 placeholder="Date"
+      />
+      <TextInput style={styles.textInput}
+                 value={report.meteo}
+                 onChangeText={(text) => handleInputChange('meteo', text)}
+                 placeholder="Météo"
+      />
+      <TextInput style={styles.textInput}
+                 value={report.temperature}
+                 onChangeText={(text) => handleInputChange('temperature', text)}
+                 placeholder="Température"
+      />
+      <TextInput style={styles.textInput}
+                 value={report.semaine}
+                 onChangeText={(text) => handleInputChange('semaine', text)}
+                 placeholder="Semaine"
+      />
+      <View>
+        <TextInput style={styles.textInput}
+                   value={report.travaux}
+                   onChangeText={(text) => handleInputChange('travaux', text)}
+                   placeholder="Travaux"
+        />
+        <TextInput style={styles.textInput}
+                   value={report.unite}
+                   onChangeText={(text) => handleInputChange('unite', text)}
+                   placeholder="Unité"
+        />
+        <TextInput style={styles.textInput}
+                   value={report.quantite}
+                   onChangeText={(text) => handleInputChange('quantite', text)}
+                   placeholder="Quantité"
+        />
+      </View>
+      <TouchableOpacity onPress={handleSubmit} style={styles.buttonSubmit}>
+        <Text style={styles.textSubmit}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,7 +94,27 @@ const HomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    borderRadius: 5,
+    padding: 10,
+  },
+  buttonSubmit: {
+    backgroundColor: 'tomato',
+    padding: 8,
+    borderRadius: 10,
+    marginBottom: 36,
+  },
+  textSubmit: {
+    color: '#fff',
+    textAlign: 'center',
   },
 });
 
-export default HomeScreen;
+export default ReportScreen;
